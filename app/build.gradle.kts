5d5f4fe102ca216170f59f472f93b0d1b6d3c183
hamzalkhalofi1 <hamzalkhalofi1@gmail.com>
2026-05-10 07:47:16 +0000

    Align lifecycle library versions

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,16 +54,20 @@ dependencies {
     implementation(platform("org.jetbrains.kotlin:kotlin-bom:1.9.0"))
     implementation("org.jetbrains.kotlin:kotlin-stdlib")
     implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
     implementation("androidx.activity:activity-compose:1.8.2")
     implementation(platform("androidx.compose:compose-bom:2023.08.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.2")
     implementation("com.google.android.material:material:1.9.0")
 
+    // Lifecycle
+    implementation(platform("androidx.lifecycle:lifecycle-bom:2.6.2"))
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx")
+    implementation("androidx.lifecycle:lifecycle-runtime-compose")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose")
 
     // Coil
     implementation("io.coil-kt:coil-compose:2.5.0")
