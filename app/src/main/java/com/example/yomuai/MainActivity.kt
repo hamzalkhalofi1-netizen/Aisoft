5f56e324d4e361157e86620c8d74183ddaca05ef
hamzalkhalofi1 <hamzalkhalofi1@gmail.com>
2026-05-10 07:00:35 +0000

    Fix missing imports in MainActivity

--- app/src/main/java/com/example/yomuai/MainActivity.kt
+++ app/src/main/java/com/example/yomuai/MainActivity.kt
@@ -19,7 +19,16 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.LibraryBooks
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material.icons.filled.Settings
-import androidx.compose.material3.*
+import androidx.compose.material3.Card
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
