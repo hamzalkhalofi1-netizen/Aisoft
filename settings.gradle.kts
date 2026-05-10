d398d355cef0d031777bc171cacefd184a02941d
hamzalkhalofi1-netizen <hamzalkhalofi1@gmail.com>
2026-05-10 10:34:06 +0100

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,11 +5,14 @@ pluginManagement {
         gradlePluginPortal()
     }
 }
+
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
+        // زدنا هاد السطر باش نضمنوا توافق كاع المكتبات الخارجية
+        maven { url = uri("https://jitpack.io") }
     }
 }
 
